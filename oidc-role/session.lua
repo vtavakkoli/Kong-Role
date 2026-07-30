6d78436f2d275ab4b3802f4aadeeec7eada6c88a
Vahid Tavakkoli <vtavakkoli@yahoo.com>
2026-07-30 21:46:33 +0200

    Return explicit session configuration results

--- oidc-role/session.lua
+++ oidc-role/session.lua
@@ -1,14 +1,18 @@
 local M = {}
 
 function M.configure(config)
-  if config.session_secret then
-    local decoded_session_secret = ngx.decode_base64(config.session_secret)
-    if not decoded_session_secret then
-      kong.log.err("Invalid plugin configuration, session secret could not be decoded")
-      return kong.response.error(ngx.HTTP_INTERNAL_SERVER_ERROR)
-    end
-    ngx.var.session_secret = decoded_session_secret
+  if not config.session_secret then
+    return true
   end
+
+  local decoded_session_secret = ngx.decode_base64(config.session_secret)
+  if not decoded_session_secret or decoded_session_secret == "" then
+    kong.log.err("Invalid plugin configuration: session secret could not be decoded")
+    return nil, "invalid session secret"
+  end
+
+  ngx.var.session_secret = decoded_session_secret
+  return true
 end
 
 return M
